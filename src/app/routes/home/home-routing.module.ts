57637ae2b8f0ad9485300d02a59cb2815bcf6545
Ashwin <ashwingairola999@gmail.com>
2021-01-14 23:00:09 +0530

    Route setup for the landing page. HomeModule and HomeComponent created.

--- src/app/routes/home/home-routing.module.ts
+++ src/app/routes/home/home-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('./routes/home/home.module').then((u) => u.HomeModule),
+    component: HomeComponent,
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class HomeRoutingModule {}
