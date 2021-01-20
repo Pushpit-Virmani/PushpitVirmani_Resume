2d423187b1076c94c3e1578a2ef415ad36613b37
Ashwin <ashwingairola999@gmail.com>
2021-01-21 02:38:40 +0530

    Added MatIconModule to HomeModule.

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HyperlinkModule } from '@pv-modules';
@@ -15,6 +16,7 @@ import { ExternalLinksComponent } from './components/external-links/external-lin
     HomeRoutingModule,
     FontAwesomeModule,
     MatButtonModule,
+    MatIconModule,
     HyperlinkModule,
   ],
 })
