8141206c4d50e2e39fa91b27600cf84427bcfce5
Ashwin <ashwingairola999@gmail.com>
2021-01-24 01:22:41 +0530

    Added toolbar and sidenav toggling logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
+import tailwindConfig from '@pv-tailwind-config';
 
 @Component({
   selector: 'pv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'pushpit-virmani-resume';
+export class AppComponent implements OnInit {
+  screens = tailwindConfig.theme.screens;
+  isSmallScreen = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(`(max-width: ${this.screens.md})`)
+      .subscribe((result) => {
+        if (result.matches) {
+          this.isSmallScreen = true;
+        }
+      });
+  }
 }
