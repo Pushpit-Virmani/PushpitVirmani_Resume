c2795613f8b780c3f98dee4342f946e0e220e459
Ashwin <ashwingairola999@gmail.com>
2021-01-24 02:36:33 +0530

    Created the SidnavLinksComponent. It displays differently for small and large screens.

---
+++ src/app/components/sidenav-links/sidenav-links.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SidenavLinksComponent } from './sidenav-links.component';
+
+describe('SidenavLinksComponent', () => {
+  let component: SidenavLinksComponent;
+  let fixture: ComponentFixture<SidenavLinksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SidenavLinksComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SidenavLinksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
