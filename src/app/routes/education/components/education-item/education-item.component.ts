9fbd5358bba022a7be6ac26eb48069a562420db9
Ashwin <ashwingairola999@gmail.com>
2021-02-16 23:05:58 +0530

    [feature/education] Created the EducationItemComponent.

---
+++ src/app/routes/education/components/education-item/education-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pv-education-item',
+  templateUrl: './education-item.component.html',
+  styleUrls: ['./education-item.component.scss']
+})
+export class EducationItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/routes/education/education.module.ts
+++ src/app/routes/education/education.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { EducationRoutingModule } from './education-routing.module';
 import { EducationComponent } from './components/education/education.component';
+import { EducationItemComponent } from './components/education-item/education-item.component';
 
 
 @NgModule({
-  declarations: [EducationComponent],
+  declarations: [EducationComponent, EducationItemComponent],
   imports: [
     CommonModule,
     EducationRoutingModule
