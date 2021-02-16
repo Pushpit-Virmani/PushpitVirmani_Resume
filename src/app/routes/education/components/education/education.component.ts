a1f66d9cbabea05d0a4e8c3c9549dea7d165518b
Ashwin <ashwingairola999@gmail.com>
2021-02-16 23:04:38 +0530

    [feature/education] Consuming the EducationApiService inside EducationComponent.

--- src/app/routes/education/components/education/education.component.ts
+++ src/app/routes/education/components/education/education.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { APIResponse, Education } from '@pv-models';
+import { Observable } from 'rxjs';
+import { EducationApiService } from 'src/app/api/education-api.service';
 
 @Component({
   selector: 'pv-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  education$?: Observable<APIResponse<Education[]>>;
 
-  constructor() { }
+  constructor(private educationApi: EducationApiService) {}
 
   ngOnInit(): void {
+    this.education$ = this.educationApi.getEducationDetails();
   }
-
 }
